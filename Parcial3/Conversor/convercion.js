37cc6847f7aa791cd6d848b0d0f8dff5c3b5ee5b
Samael-Duran <samael_drios@hotmail.com>
2021-11-15 00:00:01 -0600

    tarea

--- Parcial3/Conversor/convercion.js
+++ Parcial3/Conversor/convercion.js
@@ -1,17 +1,10 @@
 $(document).ready( function() {
-
     $("#Convertir").click(presionBoton);
-    
-    
     function presionBoton() {
-        var gra = $("#grados").val();
-    
+        var gra = $("#grados").val();    
         $.get("Mostrar.php", {grados: gra}, llegadaDatos);
-    }
-    
+    }   
     function llegadaDatos(datos) {
         $('#resultado').html('<h3>El resultado es '+datos+'</h3>');
-    }
-    
-    });
-    
+    }    
+    });   
